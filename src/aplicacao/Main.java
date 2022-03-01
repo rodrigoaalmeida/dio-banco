26c629eefaf76b5dc16e6bce3d4e6b708f91fe69
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2022-03-01 10:57:46 -0400

    Aplicacao rodando entre CC e Poupanca, mas modificacoes a frente ainda

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,11 +1,14 @@
 package aplicacao;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entidade.Conta;
 import entidade.ContaCorrente;
 import entidade.ContaPoupanca;
+import util.Operacoes;
 
 public class Main {
 
@@ -13,11 +16,18 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner leitor = new Scanner(System.in);
 		
+		Operacoes movimentacao = new Operacoes();
+		
 		
 		Conta cc = new ContaCorrente();		
 		Conta poupanca = new ContaPoupanca();
 		int controle = 1;
-		double valor = 0;
+		
+		List<Conta> tipoConta = new ArrayList<>();
+		
+		tipoConta.add(null);
+		tipoConta.add(cc);
+		tipoConta.add(poupanca);
 		
 		while(controle != 0) {
 			System.out.println(" === Bem vindo ao Banco Aqui! === ");
@@ -26,88 +36,14 @@ public class Main {
 			System.out.println("2 - Para conta Poupanca");
 			controle = leitor.nextInt();
 			
-			if(controle == 1) {
-				while(controle != 0) {
-					System.out.println("Para uma operção escolha o menu abaixo e digite:");
-					System.out.println("3 - Para deposito");
-					System.out.println("4 - Para saque");
-					System.out.println("5 - Para transferencia");
-					System.out.println("6 - Para extrato");
-					controle = leitor.nextInt();
-					
-					switch (controle) {
-					case 3:
-						System.out.println("informe o valor:");
-						valor = leitor.nextDouble();
-						cc.depositar(valor);
-						break;
-					
-					case 4:
-						System.out.println("informe o valor:");
-						valor = leitor.nextDouble();
-						cc.sacar(valor);
-						break;
-						
-					case 5:
-						System.out.println("informe o valor:");
-						valor = leitor.nextDouble();
-						cc.transferir(valor, poupanca);
-						break;
-						
-					case 6:
-						cc.imprimirExtrato();
-						break;
-					
-					case 0:
-						System.out.println("Obrigado");
-						break;
-
-					default:
-						break;
-					}
+			if (controle == 1 || controle == 2) {
+				int antControle;
+				if(controle == 1) {
+					antControle = 2;
+				} else {
+					antControle = 1;
 				}
-				controle = 1;
-			}
-			else if(controle == 2) {
-				while(controle != 0) {
-					System.out.println("Para uma operção escolha o menu abaixo e digite:");
-					System.out.println("3 - Para deposito");
-					System.out.println("4 - Para saque");
-					System.out.println("5 - Para transferencia");
-					System.out.println("6 - Para extrato");
-					controle = leitor.nextInt();
-					
-					switch (controle) {
-					case 3:
-						System.out.println("informe o valor:");
-						valor = leitor.nextDouble();
-						poupanca.depositar(valor);
-						break;
-					
-					case 4:
-						System.out.println("informe o valor:");
-						valor = leitor.nextDouble();
-						poupanca.sacar(valor);
-						break;
-						
-					case 5:
-						System.out.println("informe o valor:");
-						valor = leitor.nextDouble();
-						poupanca.transferir(valor, cc);
-						break;
-						
-					case 6:
-						poupanca.imprimirExtrato();
-						break;
-					
-					case 0:
-						System.out.println("Obrigado");
-						break;
-
-					default:
-						break;
-					}
-				}
+				movimentacao.chamar(tipoConta.get(controle), tipoConta.get(antControle));
 				controle = 1;
 			}
 			else if(controle == 0) {
@@ -116,6 +52,10 @@ public class Main {
 			else {
 				System.out.println("Operacao invalida");
 			}
+			
+			
+			
+			
 			
 		}
 		
--- src/util/Operacoes.java
+++ src/util/Operacoes.java
@@ -1,95 +1,61 @@
 package util;
 
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidade.Conta;
+
 public class Operacoes {
 
-	if(controle == 1) {
-		while(controle != 0) {
-			System.out.println("Para uma operção escolha o menu abaixo e digite:");
-			System.out.println("3 - Para deposito");
-			System.out.println("4 - Para saque");
-			System.out.println("5 - Para transferencia");
-			System.out.println("6 - Para extrato");
-			controle = leitor.nextInt();
-			
-			switch (controle) {
-			case 3:
-				System.out.println("informe o valor:");
-				valor = leitor.nextDouble();
-				cc.depositar(valor);
-				break;
-			
-			case 4:
-				System.out.println("informe o valor:");
-				valor = leitor.nextDouble();
-				cc.sacar(valor);
-				break;
-				
-			case 5:
-				System.out.println("informe o valor:");
-				valor = leitor.nextDouble();
-				cc.transferir(valor, poupanca);
-				break;
-				
-			case 6:
-				cc.imprimirExtrato();
-				break;
-			
-			case 0:
-				System.out.println("Obrigado");
-				break;
+	
+	public void chamar(Conta tipoConta, Conta antTipoConta) {
+		Locale.setDefault(Locale.US);
+		Scanner leitor = new Scanner(System.in);
 
-			default:
-				break;
-			}
-		}
-		controle = 1;
-	}
-	else if(controle == 2) {
-		while(controle != 0) {
-			System.out.println("Para uma operção escolha o menu abaixo e digite:");
-			System.out.println("3 - Para deposito");
-			System.out.println("4 - Para saque");
-			System.out.println("5 - Para transferencia");
-			System.out.println("6 - Para extrato");
-			controle = leitor.nextInt();
-			
-			switch (controle) {
-			case 3:
-				System.out.println("informe o valor:");
-				valor = leitor.nextDouble();
-				poupanca.depositar(valor);
-				break;
-			
-			case 4:
-				System.out.println("informe o valor:");
-				valor = leitor.nextDouble();
-				poupanca.sacar(valor);
-				break;
-				
-			case 5:
-				System.out.println("informe o valor:");
-				valor = leitor.nextDouble();
-				poupanca.transferir(valor, cc);
-				break;
-				
-			case 6:
-				poupanca.imprimirExtrato();
-				break;
-			
-			case 0:
-				System.out.println("Obrigado");
-				break;
+		double valor = 0;
+		int controle = 1;
 
-			default:
-				break;
-			}
-		}
-		controle = 1;
+			while (controle != 0) {
+				System.out.println("Para uma operção escolha o menu abaixo e digite:");
+				System.out.println("3 - Para deposito");
+				System.out.println("4 - Para saque");
+				System.out.println("5 - Para transferencia");
+				System.out.println("6 - Para extrato");
+				controle = leitor.nextInt();
+
+				switch (controle) {
+				case 3:
+					System.out.println("informe o valor:");
+					valor = leitor.nextDouble();
+					tipoConta.depositar(valor);
+					break;
+
+				case 4:
+					System.out.println("informe o valor:");
+					valor = leitor.nextDouble();
+					tipoConta.sacar(valor);
+					break;
+
+				case 5:
+					System.out.println("informe o valor:");
+					valor = leitor.nextDouble();
+					tipoConta.transferir(valor, antTipoConta);
+					break;
+
+				case 6:
+					tipoConta.imprimirExtrato();
+					break;
+
+				case 0:
+					System.out.println("Obrigado");
+					break;
+
+				default:
+					break;
+				}
+			}			
 	}
-	else if(controle == 0) {
-		System.out.println("Obrigado");
-	}
-	else {
-		System.out.println("Operacao invalida");
-	}
+	 
+	
 }
